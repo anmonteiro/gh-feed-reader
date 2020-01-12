b2592cd80c5c11a8abedc46b771e89c8700df942
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-11 23:50:00 -0800

    Simplify overlays, deps, build with dune 2

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,11 @@
 { pkgsPath ? <nixpkgs> }:
 
 let
-  pkgs = import pkgsPath {};
+  overlays = builtins.fetchTarball {
+    url = https://github.com/anmonteiro/nix-overlays/archive/6c6f3e1.tar.gz;
+    sha256 = "0ih9yf4dnp72g3rjqxak16mgw073ca62f0vbx61dwvhxl371vl4q";
+  };
+  pkgs = import pkgsPath { overlays = [ (import overlays) ]; };
 
   gitignoreSrc = pkgs.fetchFromGitHub {
     owner = "hercules-ci";
@@ -10,25 +14,17 @@ let
     sha256 = "1zd1ylgkndbb5szji32ivfhwh04mr1sbgrnvbrqpmfb67g2g3r9i";
   };
   inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
-
-  overlays = builtins.fetchTarball {
-    url = https://github.com/anmonteiro/nix-overlays/archive/9b82f4b.tar.gz;
-    sha256 = "1fsd3i1rz62vm2sgs3hw0srl7aiphckjc24qfc8l6p1czli2w0lw";
-  };
-
-  ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_09.overrideScope'
-    (pkgs.callPackage "${overlays}/ocaml" {});
 in
   {
     native = pkgs.callPackage ./generic.nix {
-      inherit ocamlPackages gitignoreSource;
+      inherit gitignoreSource;
     };
 
     musl64 =
-      let pkgs = import "${overlays}/static.nix" {
+      let pkgs = (import "${overlays}/static" {
         inherit pkgsPath;
         ocamlVersion = "4_09";
-      };
+      });
       in
       pkgs.callPackage ./generic.nix {
         static = true;
