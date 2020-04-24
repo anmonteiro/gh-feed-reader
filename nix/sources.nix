de3286596fbcd894926533ac03240667cec1d359
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-04-24 13:21:54 -0700

    Update sources, add release flags (#24)

--- nix/sources.nix
+++ nix/sources.nix
@@ -1,9 +1,9 @@
-{ ocamlVersion }:
+{ ocamlVersion ? "4_10" }:
 
 let
   overlays = builtins.fetchTarball {
-    url = https://github.com/anmonteiro/nix-overlays/archive/186e65d.tar.gz;
-    sha256 = "1bf2idjzryb01h1z75shhjanpqflm7dzbcv0b95rb18rvg7czyi4";
+    url = https://github.com/anmonteiro/nix-overlays/archive/ba9749d.tar.gz;
+    sha256 = "143fhjxgsbx3y0yi1kyrcw97b4fqvxg9sz5qzds323a7xvx85922";
   };
 
 in
