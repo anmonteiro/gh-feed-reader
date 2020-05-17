ea9d4c49c99ca2a498728b0373e856660b6f092d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-05-16 19:03:34 -0700

    Better bsb-shell workflow (#27)

--- bsb-shell.nix
+++ bsb-shell.nix
@@ -7,14 +7,16 @@ in
   with pkgs;
 
   mkShell {
-    buildInputs = with ocamlPackages; [ bs-platform nodejs yarn merlin reason python3 ocamlformat ];
+    buildInputs = with ocamlPackages; [
+      bs-platform
+      nodejs
+      yarn
+      merlin
+      reason
+      python3
+      ocamlformat
+    ];
 
-    shellHook = ''
-      yarn install
-      mkdir -p node_modules/.bin
-      ln -sfn ${bs-platform} node_modules/bs-platform
-      ln -sfn ${bs-platform}/bin/* ./node_modules/.bin
-      echo "bs-platform linked to $(pwd)/node_modules/bs-platform"
-    '';
+    BSB_PATH="${bs-platform}";
   }
 
