73055bb7b2f7a7413827386741a41c17e8f582cd
Joseph Price <pricejosephd@gmail.com>
2021-07-11 13:49:36 -0400

    merge frontend shell

--- shell.nix
+++ shell.nix
@@ -7,5 +7,23 @@ in
 
   mkShell {
     inputsFrom = [ (import ./nix { inherit sources; }).native ];
-    buildInputs = with ocamlPackages; [ merlin ocamlformat utop ];
+    buildInputs = with ocamlPackages;
+      [
+        merlin
+        ocamlformat
+        utop
+        melange
+        dune
+        nodejs
+        yarn
+        reason
+        python3
+      ] ++ lib.optionals stdenv.isDarwin (
+        with darwin.apple_sdk.frameworks; [
+          Cocoa
+          CoreServices
+        ]
+      );
+
+    BSB_PATH = "${ocamlPackages.melange}";
   }
